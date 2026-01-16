df4c21a94ead536b8d32bc8beda36b8e2500044b
Luis Canales Vargas <canales.vargas.luis@gmail.com>
2026-01-16 17:23:50 -0500

    estratego_maintenance_customize

---
+++ estratego_maintenance_customize/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) estratego Consulting SAC (<https://estratego.pe>).
+#
+#    For Module Support : info@estratego.pe
+#
+##############################################################################
+{
+    "name": "Maintenance Customize",
+    "version": "17.0.1.0.0",
+    "category": "maintenance",
+    "summary": "Customized fields for Maintenance.",
+    "author": "Estratego Consulting SAC",
+    "license": "LGPL-3",
+    "depends": ["maintenance", "hr", "base_address_city"],
+    "data": [
+        "views/maintenance_request_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ estratego_maintenance_customize/models/maintenance_request.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class MaintenanceRequest(models.Model):
+    _inherit = "maintenance.request"
+
+    responsible_employee_id = fields.Many2one(
+        comodel_name="hr.employee",
+        string="Responsable",
+        help="Empleado responsable de atender la solicitud.",
+    )
+    workshop_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Taller",
+        help="Proveedor / taller asignado para la atención de la solicitud.",
+    )
+    city_id = fields.Many2one(
+        comodel_name="res.city",
+        string="Lugar",
+        help="Ciudad donde se atenderá la solicitud.",
+    )
+    delivery_guide = fields.Char(
+        string="Guía de Remisión",
+        help="Número de guía de remisión asociada a la solicitud.",
+    )
