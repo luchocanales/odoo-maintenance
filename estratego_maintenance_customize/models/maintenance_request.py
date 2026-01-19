44d184c47962879622a68ce2c08d9da81e1f0515
Luis Canales Vargas <canales.vargas.luis@gmail.com>
2026-01-19 10:25:46 -0500

    estratego_maintenance_customize

--- estratego_maintenance_customize/models/maintenance_request.py
+++ estratego_maintenance_customize/models/maintenance_request.py
@@ -8,25 +8,30 @@ class MaintenanceRequest(models.Model):
     responsible_employee_id = fields.Many2one(
         comodel_name="hr.employee",
         string="Responsable",
+        tracking=True,
         help="Empleado responsable de atender la solicitud.",
     )
     supervisor_employee_id = fields.Many2one(
         comodel_name="hr.employee",
         string="Supervisor",
+        tracking=True,
         help="Empleado supervisor del mantenimiento.",
     )    
     workshop_partner_id = fields.Many2one(
         comodel_name="res.partner",
         string="Taller",
         help="Proveedor / taller asignado para la atención de la solicitud.",
+        tracking=True,
         domain=[("company_type", "=", "company"), ("parent_id", "=", False)],
     )
     city_id = fields.Many2one(
         comodel_name="res.city",
         string="Lugar",
+        tracking=True,
         help="Ciudad donde se atenderá la solicitud.",
     )
     delivery_guide = fields.Char(
         string="Guía de Remisión",
+        tracking=True,
         help="Número de guía de remisión asociada a la solicitud.",
     )
