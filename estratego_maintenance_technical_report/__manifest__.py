6e97e771935ba6d8cf599ddbef42ca05d41b3ac3
Lucho Canales <87343655+luchocanales@users.noreply.github.com>
2026-08-31 12:22:24 -0500

    estratego_maintenance_technical_report

--- estratego_maintenance_technical_report/__manifest__.py
+++ estratego_maintenance_technical_report/__manifest__.py
@@ -9,7 +9,7 @@
 ##############################################################################
 {
     "name": "Informe Técnico de Mantenimiento",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.1.1",
     "category": "maintenance",
     "summary": "Formulario de informe técnico y reporte PDF",
     "author": "Estratego Consulting SAC",
